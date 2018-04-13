6cd477bbbe27ab49c51578dfbfde0abfc620d308
Ryan Scarbery <ryan.scarbery@gmail.com>
2018-04-13 16:22:32 -0700

    add base models

--- scoreboard/models.py
+++ scoreboard/models.py
@@ -2,5 +2,23 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.core import validators
 
-# Create your models here.
+import uuid
+
+
+class Game(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+
+class Player(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+
+
+class Score(models.Model):
+    game = models.ForeignKey(Game)
+    player = models.ForeignKey(Player)
+    frame = models.IntegerField(validators=[validators.MinValueValidator(0), validators.MaxValueValidator(11)])
+    attempt = models.IntegerField(validators=[validators.MinValueValidator(1), validators.MaxValueValidator(2)])
+    value = models.IntegerField(validators=[validators.MinValueValidator(0), validators.MaxValueValidator(10)])

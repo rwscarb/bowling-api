984cd0664fdff606e7fa5c9046219216afe08062
Ryan Scarbery <ryan.scarbery@gmail.com>
2018-04-15 15:07:08 -0700

    add admin interface

--- scoreboard/models.py
+++ scoreboard/models.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.core import validators
+from django.contrib import admin
 
 import uuid
 
@@ -12,17 +13,26 @@ class Player(models.Model):
     name = models.CharField(max_length=100)
     created = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Game(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     players = models.ManyToManyField(Player)
     created = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return str(self.id)
 
-# todo: validate total score of frame is not greater than STRIKE
+
 # todo: validate 11th frame can't be submitted unless 10th is STRIKE/SPARE
 # todo: validate out of order frames, e.g. first submitted frame is frame#9
+# todo: pass backend magic numbers to frontend
 class Score(models.Model):
+    class Meta:
+        unique_together = ('game', 'player', 'frame', 'attempt')
+
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     game = models.ForeignKey(Game)
     player = models.ForeignKey(Player)
@@ -31,5 +41,18 @@ class Score(models.Model):
     value = models.IntegerField(validators=[validators.MinValueValidator(0), validators.MaxValueValidator(10)])
     created = models.DateTimeField(auto_now_add=True)
 
-    class Meta:
-        unique_together = ('game', 'player', 'frame', 'attempt')
+    def clean(self):
+        pass
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super(Score, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return '{game}/{player} F{frame:02}-A{attempt:02}-{value}'.format(
+            game=self.game_id, player=self.player_id, frame=self.frame, attempt=self.attempt, value=self.value)
+
+
+@admin.register(Score)
+class ScoreModelAdmin(admin.ModelAdmin):
+    pass
